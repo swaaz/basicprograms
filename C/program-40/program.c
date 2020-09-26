6fa08a01c11e860d3140e2e3b692e16018123daa
dk5ee <ernstpeterboehm@gmail.com>
2020-09-26 11:06:12 +0000

    fix compiler warnings

--- C/program-40/program.c
+++ C/program-40/program.c
@@ -8,7 +8,8 @@ int ack(int m,int n)
     else if(m!=0 && n==0)
       return ack(m-1,1);
     else if (m!=0 && n!=0)
-      return ack(m-1,ack(m,n-1));  
+      return ack(m-1,ack(m,n-1));
+    return 0;
 }
 
 int main()
