796d8cb752334e67cd0c9f11ba7ecfa88e8f38d7
SyedAtifShareef <160418735103@mjcollege.ac.in>
2020-10-02 17:39:45 +0530

    Update program.py
    hi sir,
    i added a comment to your program that explains why is print function used.
    thanks a lot for sharing online.

--- Python/program-1/program.py
+++ Python/program-1/program.py
@@ -1,1 +1,2 @@
 print("Hello, World!")
+#this python program uses the inbuilt function called as print(). the print function display the any strings,letter,numbers,characters or words onto the users screen.
