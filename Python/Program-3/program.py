657f9be79c0007e5bb4e61f973cac61434766cb8
Aashish2023 <63571854+Aashish2023@users.noreply.github.com>
2020-10-05 09:13:16 +0530

    Update program.py

--- Python/Program-3/program.py
+++ Python/Program-3/program.py
@@ -1,6 +1,6 @@
 
 
-a=input()
+a=input("Enter String: ")
 b=a[::-1] #String is reversed and stored in b
 if (a==b): # If a and b both are equal string is Palindrome
     print("String is Palindrome")
