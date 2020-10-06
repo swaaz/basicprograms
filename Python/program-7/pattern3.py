fe6a87f66d9c0d9093dab4d9d49c09b037e4cda3
mahi252001 <mahimabhat25@gmail.com>
2020-10-06 06:51:58 +0530

    modified the files

--- Python/program-7/pattern3.py
+++
@@ -1,8 +0,0 @@
-#pattern
-for i in range(1,6):
-    for j in range (1,6-i):
-        print(" ",end=" ")
-    for k in range(1,i+1):
-        print(k,end=" ")
-    print()
-        
--- Python/program-7/readme.md
+++
@@ -1,6 +0,0 @@
-Program to print the following pattern
-        1 
-      1 2 
-    1 2 3 
-  1 2 3 4 
-1 2 3 4 5 
