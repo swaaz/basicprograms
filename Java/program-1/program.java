f6f8e2b06cce409cbab84961ce2b6691d11ab297
keerthika15 <61348612+keerthika15@users.noreply.github.com>
2021-10-06 13:41:23 +0530

    Adding brackets for better understanding

--- Java/program-1/program.java
+++ Java/program-1/program.java
@@ -9,7 +9,9 @@ class prg {
 		int arr[]=new int[N];
 		System.out.println("Enter the elements in the array");
 		for(int i=0;i<N;i++)
+		{
 		arr[i]=sc.nextInt();
+		}
 		int min=arr[0]; // Assuming first element to be the minimum
 		int max=arr[0]; // Assuming first element to be the maximum
 		for(int i=1;i<N;i++)
