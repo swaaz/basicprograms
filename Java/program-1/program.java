fa0e65360900a4b362fa3942cd67f166cbedb77c
Krishnanand Bhat <54655236+Krxshna@users.noreply.github.com>
2022-10-08 22:51:05 +0530

    added brackets and max and min limits to cover all base cases.

--- Java/program-1/program.java
+++ Java/program-1/program.java
@@ -4,22 +4,24 @@ import java.io.*;
 class prg {
 	public static void main (String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the size of array");
+		System.out.println("Enter the size of array: ");
 		int N=sc.nextInt();
 		int arr[]=new int[N];
-		System.out.println("Enter the elements in the array");
+		System.out.println("Enter the elements in the array: ");
 		for(int i=0;i<N;i++)
 		{
 		arr[i]=sc.nextInt();
 		}
-		int min=arr[0]; // Assuming first element to be the minimum
-		int max=arr[0]; // Assuming first element to be the maximum
+		int min=Integer.MAX_VALUE; // Assuming greatest integer to be minimum
+		int max=Integer.MIN_VALUE; // Assuming lowest integer to be maximum
 		for(int i=1;i<N;i++)
 		{
-		    if(arr[i]<min)
-		    min=arr[i];
-		    if(arr[i]>max)
-		    max=arr[i];
+		    if(arr[i]<min){
+			    min=arr[i];
+		    }    
+		    if(arr[i]>max){
+			    max=arr[i];
+		    }
 		}
 		System.out.println("Minimum="+min+"\nMaximum="+max);
 	}
